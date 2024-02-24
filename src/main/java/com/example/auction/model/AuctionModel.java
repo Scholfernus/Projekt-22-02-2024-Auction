4a7b0abd5417d05aed03a1d0b818c53ab254eb92
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2024-02-24 21:58:42 +0100

    Zmiany klasy modelowej

--- src/main/java/com/example/auction/model/AuctionModel.java
+++ src/main/java/com/example/auction/model/AuctionModel.java
@@ -1,23 +1,94 @@
 package com.example.auction.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import com.example.auction.category.Category;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.DecimalMax;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.Digits;
+import jakarta.validation.constraints.NotEmpty;
+import org.antlr.v4.runtime.misc.NotNull;
+import org.springframework.lang.NonNull;
+import java.time.LocalDateTime;
+import java.math.BigDecimal;
 
 @Entity
 public class AuctionModel {
 @Id
 @GeneratedValue(strategy = GenerationType.IDENTITY)
 private Integer id;
+@NonNull
+@NotEmpty
 private String name;
+@NonNull
+@Digits(integer = 6, fraction=2)
+@DecimalMin(value = "0.01")
+@DecimalMax(value = "999999.99")
+private BigDecimal initialPrice;
+private BigDecimal currentPrice;
+private String description;
+private LocalDateTime endTime;
 
-    public AuctionModel(Integer id, String name) {
+    @NotNull
+    @ManyToOne()
+    @JoinColumn(name = "category_id")
+    private Category category;
+    public AuctionModel() {
+    }
+
+    public AuctionModel(String name, BigDecimal initialPrice, BigDecimal currentPrice, String description, LocalDateTime endTime) {
+        this.name = name;
+        this.initialPrice = initialPrice;
+        this.currentPrice = currentPrice;
+        this.description = description;
+        this.endTime = endTime;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
         this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
         this.name = name;
     }
 
-    public AuctionModel() {
+    public BigDecimal getInitialPrice() {
+        return initialPrice;
+    }
+
+    public void setInitialPrice(BigDecimal initialPrice) {
+        this.initialPrice = initialPrice;
+    }
+
+    public BigDecimal getCurrentPrice() {
+        return currentPrice;
+    }
+
+    public void setCurrentPrice(BigDecimal currentPrice) {
+        this.currentPrice = currentPrice;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDateTime getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(LocalDateTime endTime) {
+        this.endTime = endTime;
     }
 
     @Override
@@ -25,7 +96,10 @@ private String name;
         return "AuctionModel{" +
                 "id=" + id +
                 ", name='" + name + '\'' +
+                ", initialPrice=" + initialPrice +
+                ", currentPrice=" + currentPrice +
+                ", description='" + description + '\'' +
+                ", endTime=" + endTime +
                 '}';
     }
-
 }
