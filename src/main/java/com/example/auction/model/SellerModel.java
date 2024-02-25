34e257f302d9d26d50375ae576d8182e654b2d35
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2024-02-25 15:48:48 +0100

    Utworzyłem klasę modelową SellerModel

---
+++ src/main/java/com/example/auction/model/SellerModel.java
@@ -0,0 +1,51 @@
+package com.example.auction.model;
+
+import jakarta.persistence.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+public class SellerModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long sellerId;
+    private String name;
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinTable(name = "seller_auction",
+            joinColumns = @JoinColumn(name = "seller_id"),
+            inverseJoinColumns = @JoinColumn(name = "auction_id"))
+    private Set<AuctionModel> auction = new HashSet<>();
+
+    public SellerModel(String name, Set<AuctionModel> auction) {
+        this.name = name;
+        this.auction = auction;
+    }
+
+    public SellerModel() {
+    }
+
+    public Long getSellerId() {
+        return sellerId;
+    }
+
+    public void setSellerId(Long sellerId) {
+        this.sellerId = sellerId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<AuctionModel> getAuction() {
+        return auction;
+    }
+
+    public void setAuction(Set<AuctionModel> auction) {
+        this.auction = auction;
+    }
+}
