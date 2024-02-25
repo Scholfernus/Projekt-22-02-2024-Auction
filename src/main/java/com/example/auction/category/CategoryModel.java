d4d803498406e878ef9f232094e94709a5a9b306
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2024-02-25 15:48:21 +0100

    Utworzyłem klasę modelową Category

--- src/main/java/com/example/auction/category/CategoryModel.java
+++ src/main/java/com/example/auction/category/CategoryModel.java
@@ -6,7 +6,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
-public class Category {
+public class CategoryModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
