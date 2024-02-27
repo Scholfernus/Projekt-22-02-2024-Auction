dea68340f4f0b232ada488b03db88daf11693955
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2024-02-27 19:37:54 +0100

    schema i data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,14 @@
-INSERT INTO auction_model (name) VALUES ('Fiat 125P')
-INSERT INTO auction_model (name) VALUES ('Toyota')
-INSERT INTO auction_model (name) VALUES ('Mazda')
+INSERT INTO category_model(name) VALUES("Moto");
+INSERT INTO category_model(name) VALUES("Pets");
+INSERT INTO category_model(name) VALUES("Health");
+INSERT INTO category_model(name) VALUES("Tools");
+
+INSERT INTO auction_model(name, initial_price, current_price, description, end_time, category_id) VALUES("Opla sprzedam", 1.0, 1.0, "opel", "2023-09-10T09:25:12", 1);
+INSERT INTO auction_model(name, initial_price, current_price, description, end_time, category_id) VALUES("Sprzedam toyotę", 2.0, 2.0, "toyota", "2023-09-10T09:25:12", 1);
+
+
+-- INSERT INTO seller(name) VALUES("Janusz");
+
+-- INSERT INTO seller_auction(seller_id, auction_id) VALUES(1,1);
+-- INSERT INTO seller_auction(seller_id, auction_id) VALUES(1,2);
+
