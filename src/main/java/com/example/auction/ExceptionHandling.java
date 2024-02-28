3019561915c8abde51db5c1f8d8a3013af0605d3
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2024-02-28 23:54:05 +0100

    Testy, modyfikacja

--- src/main/java/com/example/auction/ExceptionHandling.java
+++ src/main/java/com/example/auction/ExceptionHandling.java
@@ -1,15 +1,22 @@
 package com.example.auction;
 
+import org.hibernate.ObjectNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 @ControllerAdvice
 public class ExceptionHandling {
+    @ExceptionHandler(ObjectNotFoundException.class)
+    public ResponseEntity<Object>handleObjectNotFoundException(ObjectNotFoundException exception){
+        return new ResponseEntity<>(exception, HttpStatus.NOT_FOUND);
+    }
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ProblemDetail handleConstrainValidationException(MethodArgumentNotValidException exception) {
         return ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, getValidationError(exception));
