6fefddcbefc385a627a040c4ac311bbca066df7e
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2024-02-28 23:54:43 +0100

    Zmiany bazodanowe

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -2,28 +2,28 @@ DROP TABLE IF EXISTS seller_auction; -- Najpierw usuń ograniczenie klucza obceg
 
 DROP TABLE IF EXISTS auction_model;
 
+DROP TABLE IF EXISTS seller_model;
+
 CREATE TABLE IF NOT EXISTS category_model (
                                               id BIGINT AUTO_INCREMENT PRIMARY KEY,
                                               name VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS seller_model (
+                                            seller_id BIGINT AUTO_INCREMENT PRIMARY KEY,
+                                            name VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS auction_model (
                                              id BIGINT NOT NULL UNIQUE AUTO_INCREMENT,
                                              seller_id BIGINT,
                                              name VARCHAR(255) NOT NULL,
-                                             initial_price FLOAT(53) NOT NULL, -- Zmieniono typ kolumny na FLOAT
-                                             current_price DOUBLE(8, 2),
+                                             initial_price FLOAT NOT NULL,
+                                             current_price DOUBLE,
                                              description VARCHAR(5000),
                                              end_time DATETIME NOT NULL,
                                              category_id BIGINT NOT NULL,
                                              FOREIGN KEY (category_id) REFERENCES category_model(id)
-);
-
-DROP TABLE IF EXISTS seller_model;
-
-CREATE TABLE IF NOT EXISTS seller_model (
-                                            seller_id BIGINT NOT NULL UNIQUE AUTO_INCREMENT,
-                                            name VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS seller_auction (
