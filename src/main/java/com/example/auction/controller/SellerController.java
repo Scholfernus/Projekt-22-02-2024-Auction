55a31f8dd4636a43b070eeb7b4e8e575c3d787c2
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2024-02-28 23:53:31 +0100

    SellerController - modyfikacja

--- src/main/java/com/example/auction/controller/SellerController.java
+++ src/main/java/com/example/auction/controller/SellerController.java
@@ -1,12 +1,13 @@
 package com.example.auction.controller;
 
+import com.example.auction.model.AuctionModel;
 import com.example.auction.model.SellerModel;
 import com.example.auction.repository.SellerRepository;
 import jakarta.validation.Valid;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.hibernate.ObjectNotFoundException;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/sellers")
@@ -21,5 +22,20 @@ public class SellerController {
     public SellerModel CreateSeller(@RequestBody @Valid SellerModel sellerModel) {
         return sellerRepository.save(sellerModel);
     }
-
+    @GetMapping("/{id}/auction")
+    public Iterable<AuctionModel> getAuctionsForSeller(@PathVariable Long id){
+        //Find if seller exist - get auctions for seller
+        //declarative style
+        return sellerRepository.findById(id)
+                .orElseThrow(()->new ObjectNotFoundException(id," seller not found"))
+                .getAuctions();
+        // poniższy kod jest opcjonalny
+//        Optional<SellerModel> sellerModel = sellerRepository.findById(id);
+//        if(sellerModel.isPresent()){
+//            SellerModel unwrapedSeller = sellerModel.get();
+//            return unwrapedSeller.getAuctions();
+//        }else {
+//            throw new ObjectNotFoundException(id, "seller not found");
+//        }
+    }
 }
