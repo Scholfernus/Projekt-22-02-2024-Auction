aa6002eca09f9eff33a9dac105ba2ebd4dc526f2
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2024-02-29 22:36:55 +0100

    Wyjątek z rzutowaniem informacji na konsoli

--- src/main/java/com/example/auction/ExceptionHandling.java
+++ src/main/java/com/example/auction/ExceptionHandling.java
@@ -3,28 +3,28 @@ package com.example.auction;
 import org.hibernate.ObjectNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import java.util.Objects;
 import java.util.stream.Collectors;
 
 @ControllerAdvice
 public class ExceptionHandling {
     @ExceptionHandler(ObjectNotFoundException.class)
-    public ResponseEntity<Object>handleObjectNotFoundException(ObjectNotFoundException exception){
-        return new ResponseEntity<>(exception, HttpStatus.NOT_FOUND);
+    public ProblemDetail handleObjectNotFoundException(ObjectNotFoundException exception) {
+        return ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, exception.getMessage());
     }
+
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ProblemDetail handleConstrainValidationException(MethodArgumentNotValidException exception) {
         return ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, getValidationError(exception));
     }
-    private String getValidationError(MethodArgumentNotValidException exception){
+
+    private String getValidationError(MethodArgumentNotValidException exception) {
         return exception.getFieldErrors()
                 .stream()
-                .map(error->error.getField() + " " + error.getDefaultMessage())
+                .map(error -> error.getField() + " " + error.getDefaultMessage())
                 .collect(Collectors.joining("; "));
     }
 }
