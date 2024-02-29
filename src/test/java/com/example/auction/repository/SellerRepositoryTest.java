2b7b6b4c69fe2deb08f0f43c8b97c7f753228f96
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2024-02-29 22:38:44 +0100

    Test SellerRepositoryTest

---
+++ src/test/java/com/example/auction/repository/SellerRepositoryTest.java
@@ -0,0 +1,32 @@
+package com.example.auction.repository;
+
+import com.example.auction.model.AuctionModel;
+import com.example.auction.model.SellerModel;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDateTime;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+class SellerRepositoryTest {
+
+    @Autowired
+    private SellerRepository repository;
+
+    @Test
+    void shouldSaveBook(){
+        //given
+        AuctionModel auction1 = new AuctionModel("test auction1",10.0,25.0,"book1", LocalDateTime.now());
+        AuctionModel auction2 = new AuctionModel("test auction2",5.0,50.0,"book2", LocalDateTime.now());
+        Set<AuctionModel> auction = Set.of(auction1);
+        SellerModel bob = new SellerModel("Bob", auction);
+        //when
+        SellerModel saved = repository.save(bob);
+        // then
+        assertEquals("bob",saved.getName());
+    }
+}
