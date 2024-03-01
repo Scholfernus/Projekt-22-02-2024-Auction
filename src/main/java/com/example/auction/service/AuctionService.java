8018cdbf0c68207b8722fe36002e0350146b4501
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2024-03-01 21:22:32 +0100

    AuctionService - dodanie pierwszych linijek do klasy serwisowej

--- src/main/java/com/example/auction/service/AuctionService.java
+++ src/main/java/com/example/auction/service/AuctionService.java
@@ -1,4 +1,23 @@
 package com.example.auction.service;
 
+import com.example.auction.model.AuctionModel;
+import com.example.auction.repository.AuctionRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class AuctionService {
+    private final AuctionRepository auctionRepository;
+
+    public AuctionService(AuctionRepository auctionRepository) {
+        this.auctionRepository = auctionRepository;
+    }
+    public List<AuctionModel> getAuctionList() {
+        return auctionRepository.findAll();
+    }
+
+    public void save(AuctionModel auctionModel) {
+        auctionRepository.save(auctionModel);
+    }
 }
