0a803aa2af2065884ce6a0012ed71734ecc7812e
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2024-03-01 21:23:16 +0100

    Poprawki w data.sql oraz schema.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -7,6 +7,7 @@ INSERT INTO auction_model(name, initial_price, current_price, description, end_t
 INSERT INTO auction_model(name, initial_price, current_price, description, end_time, category_id) VALUES("Sprzedam toyotę", 2.0, 2.0, "toyota", "2023-09-10T09:25:12", 1);
 
 
+
 INSERT INTO seller_model(name) VALUES("Janusz");
 
 INSERT INTO seller_auction(seller_id, auction_id) VALUES(1,1);
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -14,6 +14,17 @@ CREATE TABLE IF NOT EXISTS seller_model (
                                             name VARCHAR(255) NOT NULL
 );
 
+# CREATE TABLE IF NOT EXISTS auction_model (
+#                                              id BIGINT NOT NULL UNIQUE AUTO_INCREMENT,
+#                                              seller_id BIGINT,
+#                                              name VARCHAR(255) NOT NULL,
+#                                              initial_price FLOAT NOT NULL,
+#                                              current_price DOUBLE,
+#                                              description VARCHAR(5000),
+#                                              end_time DATETIME NOT NULL,
+#                                              category_id BIGINT NOT NULL,
+#                                              FOREIGN KEY (category_id) REFERENCES category_model(id)
+# );
 CREATE TABLE IF NOT EXISTS auction_model (
                                              id BIGINT NOT NULL UNIQUE AUTO_INCREMENT,
                                              seller_id BIGINT,
@@ -23,8 +34,11 @@ CREATE TABLE IF NOT EXISTS auction_model (
                                              description VARCHAR(5000),
                                              end_time DATETIME NOT NULL,
                                              category_id BIGINT NOT NULL,
+                                             FOREIGN KEY (seller_id) REFERENCES seller_model(seller_id),
                                              FOREIGN KEY (category_id) REFERENCES category_model(id)
 );
+
+
 
 CREATE TABLE IF NOT EXISTS seller_auction (
                                               seller_id BIGINT,
