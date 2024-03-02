90fefeffbebc21c1ee13a0cc2f4563ed957fb941
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2024-03-02 08:48:38 +0100

    data.sql - poprawka danych

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,13 +3,14 @@ INSERT INTO category_model(name) VALUES("Pets");
 INSERT INTO category_model(name) VALUES("Health");
 INSERT INTO category_model(name) VALUES("Tools");
 
-INSERT INTO auction_model(name, initial_price, current_price, description, end_time, category_id) VALUES("Opla sprzedam", 1.0, 1.0, "opel", "2023-09-10T09:25:12", 1);
-INSERT INTO auction_model(name, initial_price, current_price, description, end_time, category_id) VALUES("Sprzedam toyotę", 2.0, 2.0, "toyota", "2023-09-10T09:25:12", 1);
-
-
+INSERT INTO auction_model(name, initial_price, current_price, description, end_time, category_id) VALUES("Opla sprzedam", 1.0, 1.0, "opel", "2024-03-01T09:25:12", 1);
+INSERT INTO auction_model(name, initial_price, current_price, description, end_time, category_id) VALUES("Sprzedam toyotę", 2.0, 2.0, "toyota", "2024-09-10T09:25:12", 1);
 
 INSERT INTO seller_model(name) VALUES("Janusz");
+INSERT INTO seller_model(name) VALUES("Tadeusz");
 
 INSERT INTO seller_auction(seller_id, auction_id) VALUES(1,1);
+INSERT INTO seller_auction(seller_id, auction_id) VALUES(2,1);
+INSERT INTO seller_auction(seller_id, auction_id) VALUES(2,2);
 INSERT INTO seller_auction(seller_id, auction_id) VALUES(1,2);
 
