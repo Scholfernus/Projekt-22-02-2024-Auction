8c4cde45ce62114c2731eb9cc1cea3d6843c5c7a
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2024-03-02 21:21:02 +0100

    Nowa klasa testowa - AuctionRepositoryTest.java

---
+++ src/test/java/com/example/auction/repository/AuctionRepositoryTest.java
@@ -0,0 +1,38 @@
+//package com.example.auction.repository;
+//
+//import com.example.auction.model.AuctionModel;
+//import com.example.auction.model.CategoryModel;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//import java.util.List;
+//import java.util.Optional;
+//
+//import static org.junit.jupiter.api.Assertions.*;
+//@SpringBootTest
+//class AuctionRepositoryTest {
+//    @Autowired
+//    private AuctionRepository auctionRepository;
+//
+//    @Autowired
+//    private CategoryRepository categoryRepository;
+//
+//    @Test
+//    void shouldGetAuctionsByCategory() {
+//
+//        Optional<CategoryModel> motoCategory = categoryRepository.findByName("Moto");
+//
+//        List<AuctionModel> res = auctionRepository.findByCategory(motoCategory.get());
+//
+//        assertEquals(2, res.size());
+//    }
+//
+//    @Test
+//    void shouldFindAuctionByQueryString() {
+//
+//        List<AuctionModel> auctions = auctionRepository.findAuctionBy("toyota");
+//
+//        assertEquals(1, auctions.size());
+//    }
+//}
