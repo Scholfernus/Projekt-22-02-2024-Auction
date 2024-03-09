27bb467f8bd36a7a12a16cdfa50285d4d83bde75
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2024-03-09 12:08:31 +0100

    Refaktoryzacja kodu klasa Controller

--- src/main/java/com/example/auction/controller/AuctionController.java
+++ src/main/java/com/example/auction/controller/AuctionController.java
@@ -1,14 +1,8 @@
 package com.example.auction.controller;
 
-import com.example.auction.CategoryNotFoundException;
 import com.example.auction.model.AuctionModel;
-import com.example.auction.model.CategoryModel;
-import com.example.auction.repository.AuctionRepository;
-import com.example.auction.repository.CategoryRepository;
 import com.example.auction.service.AuctionService;
 import jakarta.validation.Valid;
-import org.hibernate.ObjectNotFoundException;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,75 +11,37 @@ import java.util.List;
 @RestController
 @RequestMapping("/auction")
 public class AuctionController {
-    private final AuctionRepository auctionRepository;
-    private final CategoryRepository categoryRepository;
+
     private final AuctionService auctionService;
 
-    public AuctionController(AuctionRepository auctionRepository, CategoryRepository categoryRepository, AuctionService auctionService) {
-        this.auctionRepository = auctionRepository;
-        this.categoryRepository = categoryRepository;
+    public AuctionController(AuctionService auctionService) {
         this.auctionService = auctionService;
     }
 
     @PostMapping("/add")
     public AuctionModel postAuction(@RequestBody @Valid AuctionModel modelAuction) {
-//        Optional<CategoryModel> categoryByName = categoryRepository.findByName(modelAuction.getCategory().getName());
-//        CategoryModel categoryNotFound = categoryByName
-//                .orElseThrow(() -> new ObjectNotFoundException(modelAuction, "category not found"));
-//        return getAuctionModel(modelAuction, categoryNotFound);
-        //Ta powyższa metoda została zrobiona na początku, działa tak samo jak poniżej
-
-        return categoryRepository.findByName(modelAuction.getCategory().getName())
-                .map(categoryNotFound -> getAuctionModel(modelAuction, categoryNotFound))
-                .orElseThrow(() -> new ObjectNotFoundException(modelAuction.getCategory(), "category not found"));
-    }
-
-    private AuctionModel getAuctionModel(AuctionModel modelAuction, CategoryModel categoryNotFound) {
-        modelAuction.setCategory(categoryNotFound);
-        return auctionRepository.save(modelAuction);
+        return auctionService.saveAuction(modelAuction);
     }
 
     @GetMapping()
     public List<AuctionModel> getAllAuction() {
-        return auctionRepository.findAll();
+        return auctionService.getAuctionList();
     }
-@GetMapping("/searchByCategory")
-public List<AuctionModel>getAuctionCategoryById(@RequestParam("category") String categoryName){
-//   find category by name
-    CategoryModel category = categoryRepository.findByName(categoryName).orElseThrow(() -> new CategoryNotFoundException(("category" + categoryName + " not found")));
-//    find and return auctions by category
-    return auctionRepository.findByCategory(category);
-}
+
+    @GetMapping("/searchByCategory")
+    public List<AuctionModel> getAuctionCategory(@RequestParam("category") String categoryName) {
+
+        return auctionService.getAuctionByCategory(categoryName);
+    }
+
     @PutMapping("/{id}")
-    public AuctionModel updateAuction (@PathVariable Long id, @RequestBody AuctionModel modelUpdate){
-        return auctionRepository.save(findByIdAndUpdateFields(id, modelUpdate));
-    }
-
-    private AuctionModel findByIdAndUpdateFields(Long id, AuctionModel modelUpdate) {
-        return auctionRepository.findById(id)
-                .map(auctionNotFound -> updatedAuction(modelUpdate, auctionNotFound))
-                .orElseThrow(() -> new ObjectNotFoundException(id, "auction not found "));
-    }
-
-    private static AuctionModel updatedAuction(AuctionModel modelUpdate, AuctionModel auctionNotFound) {
-        auctionNotFound.setName(modelUpdate.getName());
-        auctionNotFound.setCurrentPrice(modelUpdate.getCurrentPrice());
-        auctionNotFound.setInitialPrice(modelUpdate.getInitialPrice());
-        auctionNotFound.setDescription(modelUpdate.getDescription());
-        auctionNotFound.setEndTime(modelUpdate.getEndTime());
-        auctionNotFound.setSeller(modelUpdate.getSeller());
-        auctionNotFound.setCategory(modelUpdate.getCategory());
-        return auctionNotFound;
+    public AuctionModel updateAuction(@PathVariable Long id, @RequestBody @Valid AuctionModel modelUpdate) {
+        return auctionService.updateAuction(modelUpdate, id);
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteAuction(@PathVariable int id) {
-        if (auctionRepository.findById((long) id).isPresent()) {
-            auctionRepository.deleteById((long) id);
-            return ResponseEntity.noContent().build();
-        } else {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
-        }
+    public ResponseEntity<Object> deleteAuction(@PathVariable Long id) {
+        return auctionService.deleteAuction(id);
     }
 }
 //    @DeleteMapping("/{id}")
