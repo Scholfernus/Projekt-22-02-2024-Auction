12dc4de8f78d2b477cc50109fa9eeb895da25e5e
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2024-03-12 22:34:53 +0100

    AuctionRepository - czyszczenie kodu

--- src/main/java/com/example/auction/repository/AuctionRepository.java
+++ src/main/java/com/example/auction/repository/AuctionRepository.java
@@ -4,7 +4,6 @@ import com.example.auction.model.AuctionModel;
 import com.example.auction.model.CategoryModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.math.BigDecimal;
 import java.util.List;
 
 public interface AuctionRepository extends JpaRepository<AuctionModel, Long> {
